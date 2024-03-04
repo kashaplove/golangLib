52a9da3183374a3acbc66a67622b56f5332f4e09
kashaplove <roma20.94.a@gamil.com>
2024-03-04 21:25:10 +0300

    1.1

--- main.go
+++ main.go
@@ -1,34 +1,18 @@
 package golangLib
 
 import (
-	"fmt"
 	"math"
 )
 
-func main() {
-	fmt.Println(Sqrt(5))
+func Abs(x float64) float64 {
+	return math.Abs(x)
 }
-
-func Sqrt(x float64) float64 {
-	return math.Sqrt(x)
+func Acos(x float64) float64 {
+	return math.Acos(x)
 }
-
-func Ceil(x float64) float64 {
-	return math.Ceil(x)
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
 }
-
-func Floor(x float64) float64 {
-	return math.Floor(x)
-}
-
-func Pow(x, y float64) float64 {
-	return math.Pow(x, y)
-}
-
-func Max(x, y float64) float64 {
-	return math.Max(x, y)
-}
-
-func Min(x, y float64) float64 {
-	return math.Min(x, y)
+func Asin(x float64) float64 {
+	return math.Asin(x)
 }
